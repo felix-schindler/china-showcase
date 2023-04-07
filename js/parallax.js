0d659470cc20b314608beeaf45966b31c93794e1
Felix <felix-schindler@outlook.com>
2023-04-07 09:37:49 +0200

    Replace jquery with vanilla js

--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,12 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    $('.parallax-h1').css('top', (scrollTop * -0.5) + 'px');
-  });}else
-{
-  $(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    $('.parallax-h1').css('top', (scrollTop * -0.3) + 'px');
-    $('.parallax-about-img').css('top', (scrollTop * -0.2 ) + 'px');
-  });
+const h1 = document.querySelector('.parallax-h1');
+const about = document.querySelector('.parallax-about-img');
+
+window.onscroll = () => {
+  const scrollTop = window.scrollY;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    h1.style.top = (scrollTop * -0.5) + 'px';
+  } else {
+    h1.style.top = (scrollTop * -0.2) + 'px';
+    about.style.top = (scrollTop * -0.2) + 'px';
+  }
 }
